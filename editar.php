8d03921be71f8675ba009af094dd07d187048ce7
kevin obed mendoza <kevin.obed1999@gmail.com>
2022-12-03 10:18:20 -0500

    mejras

--- editar.php
+++ editar.php
@@ -3,8 +3,12 @@ include_once('conexion.php');
 
 $id = $_GET['id'];
 
-$query = "SELECT id, CONCAT(IFNULL(primer_nombre,''),' ',IFNULL(segundo_nombre,''),' ',IFNULL(primer_apellido,''),' ',IFNULL(segundo_apellido,'')) AS nombre_completo, id_sexo, id_ciudad, fecha_nacimiento FROM personas";
-$personas = mysqli_query($con, $query) or die(mysqli_error($con));
+$query = "SELECT * FROM personas WHERE id=($id)";
+$result = mysqli_query($con, $query) or die(mysqli_error($con));
+
+if ($result) {
+    $personas   = mysqli_fetch_assoc($result);
+}
 
 $query = "SELECT id, nombre AS id_ciudad FROM ciudades";
 $ciudades = mysqli_query($con, $query) or die(mysqli_error($con));
@@ -20,7 +24,7 @@ $ciudades = mysqli_query($con, $query) or die(mysqli_error($con));
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <title>Biblioteca - Editar Libro</title>
+    <title>Editar personas</title>
 </head>
 
 <body>
@@ -32,26 +36,26 @@ $ciudades = mysqli_query($con, $query) or die(mysqli_error($con));
                     <form action="guardar.php" method="post">
                         <div class="mb-3 col-md-4">
                             <label for="primer_nombre" class="form-label">Primer nombre</label>
-                            <input type="text" class="form-control" id="primer_nombre" name="primer_nombre" placeholder="Primer nombre" required>
+                            <input type="text" class="form-control" id="primer_nombre" name="primer_nombre" placeholder="Primer nombre" value="<?php echo $personas['primer_nombre']; ?>"  required>
                         </div>
                         <div class="mb-3 col-md-4">
                             <label for="segundo_nombre" class="form-label">Segundo nombre</label>
-                            <input type="text" class="form-control" id="segundo_nombre" name="segundo_nombre" placeholder="Segundo nombre">
+                            <input type="text" class="form-control" id="segundo_nombre" name="segundo_nombre" placeholder="Segundo nombre" value="<?php echo $personas['segundo_nombre']; ?>">
                         </div>
                         <div class="mb-3 col-md-4">
                             <label for="primer_apellido" class="form-label">Primer apellido</label>
-                            <input type="text" class="form-control" id="primer_apellido" name="primer_apellido" placeholder="Primer apellido" required>
+                            <input type="text" class="form-control" id="primer_apellido" name="primer_apellido" placeholder="Primer apellido" value="<?php echo $personas['primer_apellido']; ?>" required>
                         </div>
                         <div class="mb-3 col-md-4">
                             <label for="segundo_apellido" class="form-label">Segundo apellido</label>
-                            <input type="text" class="form-control" id="segundo_apellido" name="segundo_apellido" placeholder="Segundo apellido">
+                            <input type="text" class="form-control" id="segundo_apellido" name="segundo_apellido" placeholder="Segundo apellido" value="<?php echo $personas['segundo_apellido']; ?>">
                         </div>
                         <div class="mb-3 col-md-4">
                             <label for="sexo">Sexo</label>
                             <select class="form-select" id="id_sexo" name="id_sexo" required>
                                 <option selected>Selecciona una opccion...</option>
-                                <option value="1">Masculino</option>
-                                <option value="2">Femenino</option>
+                                <option value="1" <?= $personas['id_sexo'] == 1 ? 'selected' : '1'; ?>>Masculino</option>
+                                <option value="2" <?= $personas['id_sexo'] == 2 ? 'selected' : '2'; ?>>Femenino</option>
                             </select>
                         </div>
 
